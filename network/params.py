55756e8717ff8e08df55df4f97e877284cc69a1e
Sergey Shilin <shilinshtein@gmail.com>
2018-02-02 08:02:55 -0500

    the very first preparation submit

--- network/params.py
+++ network/params.py
@@ -1,31 +1,19 @@
-from model import get_model_vgg16_pretrained
-from model import get_model_vgg16
-from model import get_model_vgg19_pretrained
-from model import get_model_resnet50_pretrained # (197, 197, 3)
-from model import get_model_mobilenet_pretrained # (128, 128, 3)
-from model import get_model_inceptionv3_pretrained # (139, 139, 3)
-from model import get_model_xception_pretrained
-from model import get_model_custom
-from model import get_model_custom2
-from model import get_model_fft_dense
+from models import uNet_256
 
-from utils import identical
-from utils import fft
-from utils import correlation
-from utils import data_normalization
-from utils import color_composition
+from utils.data_preprocessing import identical
 
 seed = 13
-max_epochs = 1000
-batch_size = 64
-model_factory = get_model_vgg16_pretrained
+max_epochs = 100
+batch_size = 16
+model_factory = uNet_256
 
-data_adapt = identical #fft # data_normalization
-pseudolabeling = True
+data_adapt = identical
+pseudolabeling = False
 
-model_input_size = (75, 75, 3)
+model_input_size = (256, 256, 3)
 best_weights_path = 'weights/best_weights.hdf5'
 best_weights_checkpoint = 'weights/best_weights_checkpoint.hdf5'
+init_weights_path = 'weights/init_weights.hdf5'
 best_model_path = 'models/best_model.json'
 tta_steps = 10
 num_folds = 5
@@ -34,9 +22,9 @@ learning_rates = [0.001 * 3., 0.001, 0.001 / 3.]
 ## Augmentation parameters
 aug_horizontal_flip = False
 aug_vertical_flip = False
-aug_rotation = 20
+aug_rotation = 0.0
 aug_width_shift = 0.0
 aug_height_shift = 0.0
-aug_channel_shift = 0.0
-aug_shear = 0.0
-aug_zoom = 0.2
+aug_channel_shift = 0.2
+aug_shear = 0.05
+aug_zoom = 0.0
--- network/utils.py
+++ network/utils.py
@@ -5,47 +5,11 @@ from keras.preprocessing import image
 from sklearn.preprocessing import StandardScaler
 
 
-def get_data(band_1, band_2, *meta):
-    X_band_1 = np.array([np.array(band).astype(np.float32).reshape(75, 75) for band in band_1])
-    X_band_2 = np.array([np.array(band).astype(np.float32).reshape(75, 75) for band in band_2])
-
-    X_band_1_val = np.power(10, X_band_1 / 20.0)
-    X_band_2_val = np.power(10, X_band_2 / 20.0)
-
-    X_band_sqrmean = 20 * np.log10(np.sqrt(np.square(X_band_1_val) + np.square(X_band_2_val)))
-
-    data = np.concatenate(
-        [
-            X_band_1[:, :, :, np.newaxis],
-            X_band_2[:, :, :, np.newaxis],
-            X_band_sqrmean[:, :, :, np.newaxis]
-        ],
-        axis=-1
-    )
-
-    X_meta = np.concatenate(
-        [
-            np.asarray(m).astype(np.float32)[..., np.newaxis] for m in meta
-        ], 
-        axis=1
-    )
-    scaler = StandardScaler()
-
-    return data, scaler.fit_transform(X_meta)
-
+def get_data():
+    return None
 
 def identical(data):
     return data
-
-def fft(data):
-    # fft_data = np.zeros((data.shape[0], data.shape[1], data.shape[2], 3))
-    fft_data = np.zeros((data.shape[0], data.shape[1], data.shape[2], 4))
-    fft_data[..., 0] = np.real(np.fft.fft2(data[..., 0], axes = (1, 2)))
-    fft_data[..., 1] = np.imag(np.fft.fft2(data[..., 0], axes = (1, 2)))
-    # fft_data[..., 2] = np.abs(np.fft.fft2(data[..., 1], axes = (1, 2)))
-    fft_data[..., 2] = np.real(np.fft.fft2(data[..., 1], axes = (1, 2)))
-    fft_data[..., 3] = np.imag(np.fft.fft2(data[..., 1], axes = (1, 2)))
-    return fft_data.reshape(fft_data.shape[0], -1)
 
 def correlation(data):
     corr_data = np.zeros((data.shape[0], data.shape[1], data.shape[2], 3))
@@ -53,21 +17,6 @@ def correlation(data):
     corr_data[..., 1] = np.sqrt(data[..., 0] ** 2 + data[..., 1] ** 2)
     corr_data[..., 2] = np.arctan2(data[..., 0], data[..., 1])
     return corr_data
-
-def color_composition(data):
-    rgb_arrays = np.zeros(data.shape).astype(np.float32)
-    for i, data_row in enumerate(data):
-        band_1 = data_row[:,:,0]
-        band_2 = data_row[:,:,1]
-        band_3 = data_row[:,:,2]
-
-        r = (band_1 + abs(band_1.min())) / np.max((band_1 + abs(band_1.min())))
-        g = (band_2 + abs(band_2.min())) / np.max((band_2 + abs(band_2.min())))
-        b = (band_3 + abs(band_3.min())) / np.max((band_3 + abs(band_3.min())))
-
-        rgb = np.dstack((r, g, b))
-        rgb_arrays[i] = rgb
-    return np.array(rgb_arrays)
 
 def data_normalization(data):
     rgb_arrays = np.zeros(data.shape).astype(np.float32)
@@ -93,51 +42,6 @@ def get_best_history(history, monitor='val_loss', mode='min'):
 
     return best_iteration + 1, loss, acc, val_loss, val_acc
 
-# Here is the function that merges our two generators
-# We use the exact same generator with the same random seed for both the y and angle arrays
-def get_data_generator(datagen, X1, X2, y, batch_size):
-    genX1 = datagen.flow(X1, y, batch_size=batch_size, seed=55)
-    genX2 = datagen.flow(X1, X2, batch_size=batch_size, seed=55)
-
-    while True:
-        X1i = genX1.next()
-        X2i = genX2.next()
-
-        #Assert arrays are equal - this was for peace of mind, but slows down training
-        #np.testing.assert_array_equal(X1i[0],X2i[0])
-        yield [X1i[0], X2i[1]], X1i[1]
-
-def get_data_generator_test(datagen, X1, X2, batch_size):
-    genX1 = datagen.flow(X1, X2, batch_size=batch_size, shuffle=False)
-
-    while True:
-        X1i = genX1.next()
-        yield [X1i[0], X1i[1]]
-
-def get_object_size(arr):
-    p = np.reshape(np.array(arr), [75, 75]) > (np.mean(np.array(arr)) + 2 * np.std(np.array(arr)))
-    iso = p * np.reshape(np.array(arr), [75, 75])
-    return np.sum(iso < -5)
-
-def get_stats(data):
-    for i in range(2):
-        label = str(i + 1)
-        band_name = 'band_' + label
-        bands = np.array([np.array(band).astype(np.float32).reshape(75, 75) for band in data[band_name].values])
-
-        data['max_' + label] = [np.max(x) for x in bands]
-        data['maxpos_' + label] = [np.argmax(x) for x in bands]
-        data['min_' + label] = [np.min(x) for x in bands]
-        data['minpos_' + label] = [np.argmin(x) for x in bands]
-        data['med_' + label] = [np.median(x) for x in bands]
-        data['std_' + label] = [np.std(x) for x in bands]
-        data['mean_' + label] = [np.mean(np.array(x)) for x in bands]
-        data['p25_' + label] = [np.sort(x.reshape(75 * 75))[int(0.25 * 75 * 75)] for x in bands]
-        data['p75_' + label] = [np.sort(x.reshape(75 * 75))[int(0.75 * 75 * 75)] for x in bands]
-        data['mid50_' + label] = data['p75_' + label] - data['p25_' + label]
-
-    return data
-
 def resize_data(data, size):
     data_upscaled = np.zeros((data.shape[0], size[0], size[1], size[2]), dtype=data.dtype)
 
@@ -145,3 +49,4 @@ def resize_data(data, size):
         data_upscaled[i] = cv2.resize(data[i], (size[0], size[1]))
 
     return data_upscaled[:]
+
