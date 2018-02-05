862ebc1aec87415b461e1beef63166bed5639e60
Sergey Shilin <snape@liceum8.ru>
2018-02-04 21:55:37 -0500

    reduntant bce_dice was removed

--- network/losses.py
+++ network/losses.py
@@ -24,16 +24,6 @@ def dice_coeff(y_true, y_pred):
     score = (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
     return score
 
-
-def dice_coeff_hard(y_true, y_pred):
-    smooth = 1.
-    y_true_f = K.flatten(y_true)
-    y_pred_f = K.round(K.flatten(y_pred))
-    intersection = K.sum(y_true_f * y_pred_f)
-    score = (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
-    return score
-
-
 def dice_loss(y_true, y_pred):
     loss = 1 - dice_coeff(y_true, y_pred)
     return loss
