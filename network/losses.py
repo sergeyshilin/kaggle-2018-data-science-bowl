5004038842d60dae9391ac75b24327898d921a31
Sergey Shilin <shilinshtein@gmail.com>
2018-02-07 11:22:24 -0500

    updated metrics to make it working with both batches and single inputs

--- network/losses.py
+++ network/losses.py
@@ -3,25 +3,84 @@ import tensorflow as tf
 from keras.losses import binary_crossentropy
 import keras.backend as K
 
+from skimage.morphology import label
+
+
+def iou_metric(y_true_in, y_pred_in, print_table=False):
+    labels = label(y_true_in > 0.5)
+    y_pred = label(y_pred_in > 0.5)
+    
+    true_objects = len(np.unique(labels))
+    pred_objects = len(np.unique(y_pred))
+
+    intersection = np.histogram2d(labels.flatten(), y_pred.flatten(), bins=(true_objects, pred_objects))[0]
+
+    # Compute areas (needed for finding the union between all objects)
+    area_true = np.histogram(labels, bins = true_objects)[0]
+    area_pred = np.histogram(y_pred, bins = pred_objects)[0]
+    area_true = np.expand_dims(area_true, -1)
+    area_pred = np.expand_dims(area_pred, 0)
+
+    # Compute union
+    union = area_true + area_pred - intersection
+
+    # Exclude background from the analysis
+    intersection = intersection[1:,1:]
+    union = union[1:,1:]
+    union[union == 0] = 1e-9
+
+    # Compute the intersection over union
+    iou = intersection / union
+
+    # Precision helper function
+    def precision_at(threshold, iou):
+        matches = iou > threshold
+        true_positives = np.sum(matches, axis=1) == 1   # Correct objects
+        false_positives = np.sum(matches, axis=0) == 0  # Missed objects
+        false_negatives = np.sum(matches, axis=1) == 0  # Extra objects
+        tp, fp, fn = np.sum(true_positives), np.sum(false_positives), np.sum(false_negatives)
+        return tp, fp, fn
+
+    # Loop over IoU thresholds
+    prec = []
+    if print_table:
+        print("Thresh\tTP\tFP\tFN\tPrec.")
+    for t in np.arange(0.5, 1.0, 0.05):
+        tp, fp, fn = precision_at(t, iou)
+        if (tp + fp + fn) > 0:
+            p = tp / (tp + fp + fn)
+        else:
+            p = 0
+        if print_table:
+            print("{:1.3f}\t{}\t{}\t{}\t{:1.3f}".format(t, tp, fp, fn, p))
+        prec.append(p)
+    
+    if print_table:
+        print("AP\t-\t-\t-\t{:1.3f}".format(np.mean(prec)))
+    return np.mean(prec)
+
+def iou_metric_batch(y_true_in, y_pred_in):
+    batch_size = y_true_in.shape[0]
+    metric = []
+    for batch in range(batch_size):
+        value = iou_metric(y_true_in[batch], y_pred_in[batch])
+        metric.append(value)
+    return np.array(np.mean(metric), dtype=np.float32)
 
 def mean_iou(y_true, y_pred):
-    prec = []
-    for t in np.arange(0.5, 1.0, 0.05):
-        y_pred_ = tf.to_int32(y_pred > t)
-        score, up_opt = tf.metrics.mean_iou(y_true, y_pred_, 2)
-        K.get_session().run(tf.local_variables_initializer())
-        with tf.control_dependencies([up_opt]):
-            score = tf.identity(score)
-        prec.append(score)
-    return K.mean(K.stack(prec))
+    metric_value = tf.py_func(iou_metric_batch, [y_true, y_pred], tf.float32)
+    return metric_value
 
-def mean_iou_list(y_true, y_pred):
-    iou = 0.
-    for idx in range(len(y_true)):
-        y_true_tf = tf.convert_to_tensor(y_true[idx], np.float32)
-        y_pred_tf = tf.convert_to_tensor(y_pred[idx], np.float32)
-        iou += mean_iou(y_true_tf, y_pred_tf)
-    return iou / float(len(y_true))
+# def mean_iou(y_true, y_pred):
+#     prec = []
+#     for t in np.arange(0.5, 1.0, 0.05):
+#         y_pred_ = tf.to_int32(y_pred > t)
+#         score, up_opt = tf.metrics.mean_iou(y_true, y_pred_, 2)
+#         K.get_session().run(tf.local_variables_initializer())
+#         with tf.control_dependencies([up_opt]):
+#             score = tf.identity(score)
+#         prec.append(score)
+#     return K.mean(K.stack(prec))
 
 
 def dice_coeff(y_true, y_pred):
@@ -36,18 +95,18 @@ def dice_loss(y_true, y_pred):
     loss = 1 - dice_coeff(y_true, y_pred)
     return loss
 
+def bce_dice_loss_batch(y_true, y_pred):
+    batch_size = y_true_in.shape[0]
+    metric = []
+    for batch in range(batch_size):
+        value = binary_crossentropy(y_true_in[batch], y_pred_in[batch]) + \
+            dice_loss(y_true_in[batch], y_pred_in[batch])
+        metric.append(value)
+    return np.array(np.mean(metric), dtype=np.float32)
 
 def bce_dice_loss(y_true, y_pred):
-    loss = binary_crossentropy(y_true, y_pred) + dice_loss(y_true, y_pred)
+    loss = tf.py_func(bce_dice_loss_batch, [y_true, y_pred], tf.float32)
     return loss
-
-def bce_dice_loss_list(y_true, y_pred):
-    loss = 0.
-    for idx in range(len(y_true)):
-        y_true_tf = tf.convert_to_tensor(y_true[idx], np.float32)
-        y_pred_tf = tf.convert_to_tensor(y_pred[idx], np.float32)
-        loss += bce_dice_loss(y_true_tf, y_pred_tf)
-    return loss / float(len(y_true))
 
 
 def weighted_dice_coeff(y_true, y_pred, weight):
