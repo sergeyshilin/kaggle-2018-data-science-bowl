b2f65005ccbd5f2f3eab30fdfdfb5525fa11c07d
Sergey Shilin <snape@liceum8.ru>
2018-02-12 00:54:32 -0500

    use right rle_encode function

--- network/utils.py
+++ network/utils.py
@@ -110,17 +110,34 @@ def get_predictions_upsampled(predictions, original_size):
     return preds_test_upsampled
 
 
-# https://www.kaggle.com/stainsby/fast-tested-rle
+# https://www.kaggle.com/stainsby/fast-tested-rle-and-input-routines
 def run_length_encode(mask):
-    '''
-    img: numpy array, 1 - mask, 0 - background
-    Returns run length as string formated
-    '''
-    inds = mask.flatten()
-    runs = np.where(inds[1:] != inds[:-1])[0] + 2
-    runs[1::2] = runs[1::2] - runs[:-1:2]
-    rle = ' '.join([str(r) for r in runs])
+    pixels = mask.T.flatten()
+    # We need to allow for cases where there is a '1' at either end of the sequence.
+    # We do this by padding with a zero at each end when needed.
+    use_padding = False
+    if pixels[0] or pixels[-1]:
+        use_padding = True
+        pixel_padded = np.zeros([len(pixels) + 2], dtype=pixels.dtype)
+        pixel_padded[1:-1] = pixels
+        pixels = pixel_padded
+    rle = np.where(pixels[1:] != pixels[:-1])[0] + 2
+    if use_padding:
+        rle = rle - 1
+    rle[1::2] = rle[1::2] - rle[:-1:2]
+    rle = ' '.join(str(x) for x in rle)
     return rle
+
+# https://www.kaggle.com/stainsby/fast-tested-rle-and-input-routines
+def run_length_decode(rle_str, mask_shape, mask_dtype):
+    s = rle_str.split()
+    starts, lengths = [np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])]
+    starts -= 1
+    ends = starts + lengths
+    mask = np.zeros(np.prod(mask_shape), dtype=mask_dtype)
+    for lo, hi in zip(starts, ends):
+        mask[lo:hi] = 1
+    return mask.reshape(mask_shape[::-1]).T
 
 
 def probas_to_rles(x, cutoff=0.5):
